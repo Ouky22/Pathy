df8bfefb0d2a6d645ac87a3b84d3cd1e87d2794e
Ouky22 <menzelmike123@gmail.com>
2024-05-04 22:10:27 +0200

    refactor: make NodeStateChange extend Equatable

--- lib/feature/pathfinding_visualizer/domain/model/node_state_change.dart
+++ lib/feature/pathfinding_visualizer/domain/model/node_state_change.dart
@@ -1,8 +1,13 @@
+import 'package:equatable/equatable.dart';
+
 import 'node_state.dart';
 
-class NodeStateChange {
-  NodeState newState;
-  int row, column;
+class NodeStateChange extends Equatable {
+  final NodeState newState;
+  final int row, column;
 
-  NodeStateChange(this.newState, this.row, this.column);
+  const NodeStateChange(this.newState, this.row, this.column);
+
+  @override
+  List<Object?> get props => [newState, row, column];
 }
